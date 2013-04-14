dc8db1b73a32f72b539df625c639514017671d96
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2013-04-14 23:16:50 +0200

    Make install

---
+++ _downloads/mixing.py
@@ -0,0 +1,36 @@
+import numpy as np
+from skimage import io
+from skimage import filter
+from skimage import morphology
+from glob import glob
+import matplotlib.pyplot as plt
+
+def mixing_region(filename):
+    white = io.imread(filename)
+    val = filter.threshold_otsu(white)
+    light_mask = white > val
+    regions = morphology.label(light_mask)
+    index_large_region = np.argmax(np.bincount(regions.ravel()))
+    fluid_mask = regions == index_large_region
+    fluid_mask = morphology.binary_erosion(fluid_mask, selem=np.ones((3, 3)))
+    return fluid_mask
+
+
+mixing_list = glob('../mixing_images/*.JPG')
+mixing_list.sort()
+
+white_list = glob('../white_images/*.JPG')
+white_list.sort()
+
+# Compute fluid region
+# ---------------------------------
+
+white = io.imread(white_list[0])
+val = filter.threshold_otsu(white)
+light_mask = white > val
+regions = morphology.label(light_mask)
+index_large_region = np.argmax(np.bincount(regions.ravel()))
+fluid_mask = regions == index_large_region
+fluid_mask = morphology.binary_erosion(fluid_mask, selem=np.ones((3, 3)))
+
+
---
+++ _downloads/plot_mixing.py
@@ -0,0 +1,5 @@
+import numpy as np
+from skimage import io
+from glob import glob
+
+# white_images = 
